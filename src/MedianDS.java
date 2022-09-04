a7814f001cbae14340d5c085c0a5674bd4b6bdaf
yogev <yogevnamir97@gmail.com>
2022-09-04 12:49:31 +0300

    first part done second in progress

--- src/MedianDS.java
+++ src/MedianDS.java
@@ -1,28 +1,109 @@
 public class MedianDS {
     LinkedList list = null;
+    MaxTreeHeap maxTreeHeap;
+    MinTreeHeap minTreeHeap;
     public MedianDS(int[] A){
-        LinkedList[] sets = divideTo5(A);
-        int[] setsMedians = new int[sets.length];
-        for(int i=0; i< sets.length; i++){
-            int[] tmp = sets[i].toArray();
-            MergeSort merge = new MergeSort();
-            merge.sort(tmp,0,tmp.length-1);
-            setsMedians[i] = tmp[tmp.length/2];
-        }
-        //MaxTreeHeap maxHeap = new MaxTreeHeap();
-        //MinTreeHeap minHeap = new MinTreeHeap();
+        int median = select(A,A.length/2);
+        int[] min, max = new int[A.length/2];
+        if(A.length%2==0)
+            min = new int[A.length/2];
+        else min = new int[(A.length/2)+1];
+        copyArray(A,max,0, max.length);
+        copyArray(A,min,max.length, A.length);
+        maxTreeHeap = MaxTreeHeap.BuildHeapT(max);
+        minTreeHeap = MinTreeHeap.BuildHeapT(min);
     }
     public void insert(int x){
 
     }
-    public void delMedian(){
 
+    public void delMedian(){
+        maxTreeHeap.HeapExtractMax();
+        if(minTreeHeap.heapSize==maxTreeHeap.heapSize+1){
+            int median = minTreeHeap.HeapExtractMin();
+            maxTreeHeap.HeapInsert(median);
+        }
     }
     public int findMedian(){
+        return maxTreeHeap.root.key;
+    }
+    public int partition(int[] A,int p, int r, int x){
+        int n = r - p;
+        int[] B = new int[n];
+        int left = 0, right = n;
+
+        for(int i=0; i<n; i++){
+            if(A[i+p]>x){
+                B[right-1] = A[i+p];
+                right--;
+            } else if (A[i+p]<x) {
+                B[left] = A[i+p];
+                left++;
+            }
+        }
+        int q = left;
+        B[q]=x;
+        for (int i=0;i<n;i++){
+            A[i+p]=B[i];
+        }
+        return p+q-1;
+    }
+    public int select(int[] A, int i){
+        if(A.length==1)
+            return A[0];
+        LinkedList[] sets = divideTo5(A);
+        int[] setsMedians = new int[sets.length];
+        MergeSort merge = new MergeSort();
+        for(int j=0; j<sets.length;j++) {
+            int[] tmp = sets[j].toArray();
+            setsMedians[j] = merge.median(tmp);
+        }
+        int x = select(setsMedians,i);
+        int q = partition(A,0, A.length-1, x);
+        if(i==q) return x;
+        else if (i<q) {
+            int[] B = new int[q-1];
+            copyArray(A,B,0,q-1);
+            select(B,i);
+        }
+        else {
+            int[] B = new int[q-1];
+            copyArray(A,B,q+1, A.length);
+            select(B,i-q);
+        }
 
         return 0;
     }
+    public void copyArray(int[] A,int[] B,int l,int r){
+        for(int i=l; i<r;i++)
+            B[i]=A[i];
+    }
+    /*
+    public int selectGivenMedian(int[] A, int i, MergeSort merge){
+        int n = A.length;
+        if(n==1)
+            return A[0];
+        int x = merge.median(A);
+        int q = partition(A,0,n,x);
 
+        if(i==q)
+            return A[q];
+        int[] B;
+        if(i<q) {
+            B = new int[q];
+            for(int j=0; j<q; j++)
+                B[j]=A[j];
+            return selectGivenMedian(B,i,merge);
+        }
+        else {
+            B = new int[n - q];
+            for(int j=q; j<n; j++)
+                B[j]=A[j];
+            return selectGivenMedian(B,i-q,merge);
+        }
+    }
+
+     */
     public LinkedList[] divideTo5(int[] A){
         LinkedList[] sets;
 
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,7 +1,7 @@
 class MergeSort {
 
-    void merge(int arr[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
+    void merge(int A[], int l, int m, int r) {
+        // Find sizes of two sub-arrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
@@ -11,23 +11,23 @@ class MergeSort {
 
         /*Copy data to temp arrays*/
         for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
+            L[i] = A[l + i];
         for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
+            R[j] = A[m + 1 + j];
 
         /* Merge the temp arrays */
 
-        // Initial indexes of first and second subarrays
+        // Initial indexes of first and second sub-arrays
         int i = 0, j = 0;
 
         // Initial index of merged subarray array
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
-                arr[k] = L[i];
+                A[k] = L[i];
                 i++;
             } else {
-                arr[k] = R[j];
+                A[k] = R[j];
                 j++;
             }
             k++;
@@ -35,32 +35,36 @@ class MergeSort {
 
         /* Copy remaining elements of L[] if any */
         while (i < n1) {
-            arr[k] = L[i];
+            A[k] = L[i];
             i++;
             k++;
         }
 
         /* Copy remaining elements of R[] if any */
         while (j < n2) {
-            arr[k] = R[j];
+            A[k] = R[j];
             j++;
             k++;
         }
     }
     // Main function that sorts arr[l..r] using
     // merge()
-    void sort(int[] arr, int l, int r)
+    void sort(int[] A, int l, int r)
     {
         if (l < r) {
             // Find the middle point
             int m =l+ (r-l)/2;
 
             // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
+            sort(A, l, m);
+            sort(A, m + 1, r);
 
             // Merge the sorted halves
-            merge(arr, l, m, r);
+            merge(A, l, m, r);
         }
+    }
+    public int median(int[] A){
+        sort(A,0,A.length-1);
+        return A[A.length/2];
     }
 }
